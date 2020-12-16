d9c4d423c09aa1474e1c32b953db70e321e37dfd
JamieCurnow <jamie@island-web.ca>
2020-12-16 13:10:28 +0000

    Hello world

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import Clipboard from 'clipboard'
+
+interface Options {
+  /** Fixes IE by appending element to body */
+  appendToBody: boolean
+}
+
+export default (opts?: Options) => {
+  // default appendToBody true
+  const appendToBody = opts?.appendToBody === undefined ? true : opts.appendToBody
+  return {
+    toClipboard(text: string, container?: HTMLElement) {
+      return new Promise((resolve, reject) => {
+        // make fake element
+        const fakeEl = document.createElement('button')
+        // setup a new Clipboard.js
+        const clipboard = new Clipboard(fakeEl, {
+          text: () => text,
+          action: () => 'copy',
+          container: container !== undefined ? container : document.body
+        })
+        clipboard.on('success', (e) => {
+          clipboard.destroy()
+          resolve(e)
+        })
+        clipboard.on('error', (e) => {
+          clipboard.destroy()
+          reject(e)
+        })
+        // appendToBody fixes IE
+        if (appendToBody) document.body.appendChild(fakeEl)
+        // simulate click
+        fakeEl.click()
+        // remove from body if appended
+        if (appendToBody) document.body.removeChild(fakeEl)
+      })
+    }
+  }
+}
